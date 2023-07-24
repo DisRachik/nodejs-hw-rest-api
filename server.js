c28df103cda987dfe84da386da82ad96c5b5629a
DisRachik <radchenkoig@gmail.com>
2023-07-24 19:20:16 +0300

    feat: add connection to mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+const { connect } = require('mongoose');
+require('dotenv').config();
+require('colors');
+
 const app = require('./app');
-// user01
-// v3OC6UN4SOvIKERa
-app.listen(3000, () => {
-  console.log('Server running. Use our API on port: 3000');
-});
+
+const { DB_HOST, PORT } = process.env;
+
+connect(DB_HOST)
+  .then((db) => {
+    console.log(
+      `Database connection successful. Port: ${db.connection.port}. Name: ${db.connection.name}. Host: ${db.connection.host}`
+        .blue.italic.bold
+    );
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`.green.italic.bold);
+    });
+  })
+  .catch((err) => {
+    console.log(`Server not running. Error message: ${err.message}`.red.italic.bold);
+    process.exit(1);
+  });
