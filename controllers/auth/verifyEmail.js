6394695d59629f4522c5b1963f0d705e5a7f61fa
DisRachik <radchenkoig@gmail.com>
2023-08-14 16:07:18 +0300

    fix: verificationToken validation

--- controllers/auth/verifyEmail.js
+++ controllers/auth/verifyEmail.js
@@ -3,13 +3,13 @@ const { HttpError } = require('../../helpers');
 
 const verifyEmail = async (req, res) => {
   const { verificationToken } = req.params;
-  const { _id } = await UsersModel.findOne({ verificationToken });
+  const user = await UsersModel.findOne({ verificationToken });
 
-  if (!_id) {
+  if (!user) {
     throw HttpError(404, 'User not found');
   }
 
-  await UsersModel.findByIdAndUpdate(_id, { verify: true, verificationToken: '' });
+  await UsersModel.findByIdAndUpdate(user._id, { verify: true, verificationToken: '' });
 
   res.status(200).json({
     status: 'success',
